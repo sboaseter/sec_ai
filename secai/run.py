22711c79a896947766a09c298d93a425832de36a
sboaseter <boaseter@gmail.com>
2017-05-20 15:16:27 -0700

    misc

--- secai/run.py
+++ secai/run.py
@@ -2,17 +2,19 @@
 #sys.path.append(os.path.join(os.path.dirname(__file__), "../secai"))
 #print(sys.path)
 from secai.models.dbmodels import TestSEC
-from colorama import init, Fore
+from colorama import init, Fore, Style
 init()
 
-print('{}{}{}'.format(Fore.GREEN, 'SEC A.I', Fore.RESET))
+print('{}{}{}{}{}'.format(Style.BRIGHT,Fore.GREEN, 'SEC A.I', Fore.RESET,Style.RESET_ALL))
+filing_type = '8-k'
+filing_num = 10
 
-x = TestSEC('https://www.sec.gov/cgi-bin/browse-edgar?company=&CIK=&type=8-k&owner=include&count=10&action=getcurrent')
+x = TestSEC('https://www.sec.gov/cgi-bin/browse-edgar?company=&CIK=&type='+filing_type+'&owner=include&count='+str(filing_num)+'&action=getcurrent')
 xres = x.scrape()
 print(str(len(xres)) + ' links')
 
 for x in xres:
-	print(x)
+	print(str(Style.DIM), Fore.YELLOW + str(x) + Fore.RESET + (Style.RESET_ALL))
 
 
 
